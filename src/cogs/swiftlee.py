9ba16711243f3303a35d3a3d789bd1335bccfd09
Jonathan Conlin <jconlin@ufl.edu>
2019-09-30 17:28:24 -0400

    added setup method

--- src/cogs/swiftlee.py
+++ src/cogs/swiftlee.py
@@ -4,6 +4,13 @@ from discord.ext import commands
 
 class SwiftleesCog(discord.Cog, name="Swiftlee's Cog"):
 
+    def __init__(self, bot):
+        self.bot = bot
+
     @commands.command()
     async def swiftlee(self, ctx):
         await ctx.channel.send(f'Woah, you sent a message in {ctx.channel.name}!')
+
+
+def setup(bot):
+    bot.add_cog(SwiftleesCog(bot))
