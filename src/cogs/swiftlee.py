f5a540120587d4e536088441de2b3cfa730cffa7
Jonathan Conlin <jconlin@ufl.edu>
2019-09-30 17:31:28 -0400

    sorry

--- src/cogs/swiftlee.py
+++ src/cogs/swiftlee.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 
-class SwiftleesCog(discord.Cog, name="Swiftlee's Cog"):
+class SwiftleesCog(commands.Cog, name="Swiftlee's Cog"):
 
     def __init__(self, bot):
         self.bot = bot
